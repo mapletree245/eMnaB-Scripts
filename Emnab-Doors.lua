e70b519eff09f54bdc0228f24459f613df8c44df
mapletree245 <131200777+mapletree245@users.noreply.github.com>
2023-08-20 16:59:33 +0100

    Emnab-Doors.lua

--- Emnab-Doors.lua
+++ Emnab-Doors.lua
@@ -1,2 +1,17 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("DOORS", "Midnight")
+local Window = Library.CreateLib("DOORS", "BloodTheme")
+
+--plr
+local ws = 15
+local player = Window:NewTab("LocalPlayer")
+local plrChanges = player:NewSection("Player Changes")
+
+plrChanges:NewSlider("Walkspeed", "SPEEEED!!", 21, 15, function(speed) -- 500 (MaxValue) | 0 (MinValue)
+    ws = speed
+end)
+
+while true do
+  wait()
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = ws
+    end
+
