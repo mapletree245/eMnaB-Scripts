33d98e4bce7a509cf7d2805da9f170583a77ade3
mapletree245 <131200777+mapletree245@users.noreply.github.com>
2023-08-20 17:04:48 +0100

    Emnab-Doors.lua

--- Emnab-Doors.lua
+++ Emnab-Doors.lua
@@ -10,6 +10,17 @@ plrChanges:NewSlider("Walkspeed", "SPEEEED!!", 21, 15, function(speed) -- 500 (M
     ws = speed
 end)
 
+plrChanges:NewToggle("God Mode", "Harmless Rush & Ambush", function(state)
+    if state then
+        local Col = game.Players.LocalPlayer.Character:FindFirstChild("Collision")
+        Col.Position = Col.Position - Vector3.new(0,10,0) 
+
+    else
+        local Col = game.Players.LocalPlayer.Character:FindFirstChild("Collision")
+        Col.Position = Col.Position + Vector3.new(0,10,0) 
+    end
+end)
+
 while true do
   wait()
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = ws
