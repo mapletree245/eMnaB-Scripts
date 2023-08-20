8a7684bf57ca18bb21bb5c2b798f412ed50d0dbb
mapletree245 <131200777+mapletree245@users.noreply.github.com>
2023-08-20 14:06:22 +0100

    Emnab-Doors.lua

--- Emnab-Doors.lua
+++ Emnab-Doors.lua
@@ -1,9 +1,3 @@
 if game.PlaceID == 6839171747 then
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-  local Window = Library.CreateLib("Doors", "Midnight")
-else
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-  local Window = Library.CreateLib("Doors", "Midnight")
-  local tab = Window:NewTab("Error")
-  tab:NewLabel("Error: Player not in doors")
+https://pastebin.com/raw/gW3xadRV
 end
